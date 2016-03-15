ba3386e964b4e5075efc7072c5eef675b260057b
AlbertThie <alb0rt@gmail.com>
2016-03-15 16:53:08 +0100

    add attacker moves

--- cyber security pilot/src/DAction.java
+++ cyber security pilot/src/DAction.java
@@ -3,8 +3,11 @@ public class DAction extends Action {
 	public DAction() {
 		// TODO Auto-generated constructor stub
 	}
-	public void AddDetect(Node n, int cost) {
-		n.setDetect(n.getDetect() + cost);
+	public void AddDetect(Node n, int invest) {
+		n.setDetect(n.getDetect() + invest);
 		
+	}
+	public void Adddefence(Node n, int invest ){
+		n.setDef(n.getDef() + invest);
 	}
 }
