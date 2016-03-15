4bece6fbba3de310fbfddbc43eb1e649f68db374
AlbertThie <alb0rt@gmail.com>
2016-03-15 14:27:01 +0100

    add defender actions

---
+++ cyber security pilot/src/DAction.java
@@ -0,0 +1,10 @@
+
+public class DAction extends Action {
+	public DAction() {
+		// TODO Auto-generated constructor stub
+	}
+	public void AddDetect(Node n, int cost) {
+		n.setDetect(n.getDetect() + cost);
+		
+	}
+}
